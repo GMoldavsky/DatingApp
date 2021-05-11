aa41761ea103752897354c3c71b4ee3eb28be616
Gregory Moldavsky <GMoldavsky@hotmail.com>
2021-05-11 11:15:45 -0400

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,18 +13,20 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        public readonly DataContext _context ;
+        private readonly DataContext _context ;
         public UsersController(DataContext context)
         {
             _context = context;
         }
-
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
---
+++ API/Services/TockenService.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TockenService : ITockenService
+    {
+        private readonly SymmetricSecurityKey _key; //One key to sign and decrypt. We can use it because key never leave the server
+        public TockenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TockenKey"]));
+        }
+
+        public string CreateTocken(AppUser user)
+        {
+            var claims = new List<Claim>{
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+                //new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+            };
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = System.DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
